0d57ce344d89a6b5a84bf9816e30d4cccde55d83
Nougat0 <90332022+Nougat0@users.noreply.github.com>
2023-03-22 18:03:36 +0900

    23.03.22 커밋

--- ui/src/main/java/com/enssel/verbena/ui/controller/Page1Controller.java
+++ ui/src/main/java/com/enssel/verbena/ui/controller/Page1Controller.java
@@ -8,11 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -38,15 +38,26 @@ public class Page1Controller {
 //	}
 	
 	@RequestMapping("/table")
-	public ResponseEntity<Object> getTableRow() {
-		
+	public ResponseEntity<Object> getTableRow(@RequestBody(required=false) String json) {
+		System.out.println("검색폼json: "+json);
+		if(json == null) { //검색 안 넘길 시
+			Object result =  restTemplate.getForObject("/page1/table", Object.class); //O
+			//Gson gson = new Gson();
+			//List<Map<String,Object>> jsonObject = gson.fromJson(result, new TypeToken<Object>(){}.getType());
+			
+			return new ResponseEntity<Object>( result, HttpStatus.OK);
+		}
+		else { //검색할 것 넘길 시 - String json 은 넘어온 검색 항목들을 포함하고 있음
+			Gson gson = new Gson();
+			Map<String, Object> jsonObject = gson.fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+			Object result =  restTemplate.postForObject("/page1/table", jsonObject ,Object.class); //O
+			//Gson gson = new Gson();
+			//List<Map<String,Object>> jsonObject = gson.fromJson(result, new TypeToken<Object>(){}.getType());
+			
+			return new ResponseEntity<Object>( result, HttpStatus.OK);			
+		}
 
 		
-		Object result =  restTemplate.getForObject("/page1/table", Object.class); //O
-		//Gson gson = new Gson();
-		//List<Map<String,Object>> jsonObject = gson.fromJson(result, new TypeToken<Object>(){}.getType());
-		
-		return new ResponseEntity<Object>( result, HttpStatus.OK);
 	}	
 	
 	@PostMapping("/regi")
@@ -60,25 +71,37 @@ public class Page1Controller {
 		return new ResponseEntity<Object>(result, HttpStatus.OK);
 	}	
 	
-	@RequestMapping("/update")
+	@RequestMapping(value="/update")
 	public ResponseEntity<Object> updateMember(@RequestBody String json) throws JsonProcessingException {
 		//String -> Map
 		System.out.println("memberJSON:"+json);
 		
 		Gson gson = new Gson();
-		Map<String, Object> jsonObject = gson.fromJson(json,new TypeToken<Map<String, Object>>(){}.getType());
+		//Map<String,Object> jsonObject = new HashMap<>();
+		//Map<String, Object> jsonObject = gson.fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+		Map<String, Object> jsonObject = gson.fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
+		//jsonObject = gson.fromJson(json, jsonObject.getClass());
 		System.out.println("UI/page1/update 컨트롤러에서 확인: "+jsonObject);
 		
 		Object result = restTemplate.postForObject("/page1/update", jsonObject, String.class);
 		return new ResponseEntity<Object>(result, HttpStatus.OK);
 	}	
 	@RequestMapping("/delete")
-	public ResponseEntity<Object> deleteMember(String[] keys){
+	public ResponseEntity<Object> deleteMember(@RequestParam(value="key[]") String[] keys){
 		System.out.println("UI/page1/delete 입력된 key값 배열 컨트롤러에서 확인: "+keys);
 		Object result = restTemplate.postForEntity("/page1/delete", keys, String.class);
 		return new ResponseEntity<Object>(result, HttpStatus.OK);
 	}	
-
+	
+//	@RequestMapping("/search")
+//	public ResponseEntity<Object> searchMember(@RequestBody String json) {
+//		Gson gson = new Gson();
+//		Map<String, Object> jsonObject = gson.fromJson(json,new TypeToken<Map<String, Object>>(){}.getType());
+//		System.out.println("UI/page1/update 컨트롤러에서 확인: "+jsonObject);
+//		
+//		Object result = restTemplate.postForObject("/page1/search", jsonObject, String.class);
+//		return new ResponseEntity<Object>(result, HttpStatus.OK);
+//	}
 	
 	
 }
